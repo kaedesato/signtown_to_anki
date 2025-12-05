218885c55396fe7a56b8e1bc5d1ac4dfbf5520b4
Kaede Sato <main@kaedesato.work>
2025-12-05 22:49:19 +0900

    250MB対応②

--- src/signtown_to_anki/main.py
+++ src/signtown_to_anki/main.py
@@ -88,7 +88,7 @@ def download_video(url, filename):
     cmd = [
         "ffmpeg", "-i", url,
         "-vcodec", "libsvtav1",
-        "-crf", "25",
+        "-crf", "30",
         "-b:v", "0",
         "-preset", "4",
         "-pix_fmt", "yuv420p",
